d23b34cd11168552dea63a730a88c5732865735f
dm036128 <122043979+dm036128@users.noreply.github.com>
2023-05-02 10:27:48 -0600

    Update list.py

--- list.py
+++ list.py
@@ -1,20 +1,19 @@
-
 import time as t
-import os as o
+import os
 todo = ["",""]
 num = 1
 def do():
-    o.system('cls')
+    os.system('cls')
     global num, todo
     t.sleep(0.5)
-    print("This is your To-Do list:")
+    print("Here is your To-Do list:")
     num = 1
     for i in todo:
         print(str(num)+": "+i)
         num+=1
     num =1
     t.sleep(0.5)
-    print("Choose one thing to do.\nChange\t1\nAdd\t2\nRemove\t3")
+    print("Options.\nChange\t1\nAdd\t2\nRemove\t3")
     inp=input("")
     if inp == "1":
         change()
@@ -26,28 +25,28 @@ def do():
         do()
 def change():
     global todo, num
-    o.system('cls')
+    os.system('cls')
     t.sleep(0.5)
-    print("This is your To-Do list:")
+    print("Here is your To-Do list:")
     for i in todo:
         print(str(num)+": "+i)
         num+=1
-    print("What number would you like to change?")
+    print("What would you like to change?")
     inp = input("")
-    todo[int(inp)-1] = input("What would you like it to be instead?\n")
+    todo[int(inp)-1] = input("Change it to?\n")
     do()
 def add():
     global todo, num
-    o.system('cls')
+    os.system('cls')
     t.sleep(0.5)
-    print("What would you like to add?")
+    print("Add what?")
     inp = input()
     todo.append(inp)
     do()
 def remove():
     global todo, num
     t.sleep(0.5)
-    print("Which item would you like to remove?")
+    print("Remove what?")
     inp=input()
     print("Type the exact text to confirm: ")
     inp = input()
